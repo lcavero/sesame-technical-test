b33c62f74e4f6b8d85a83fe030d492a6f88ecbf9
Luis Cavero <luiscaverodeveloper@gmail.com>
2023-06-14 20:00:10 +0200

    remove var_dump

--- src/Shared/Infrastructure/Exception/ApiExceptionListener.php
+++ src/Shared/Infrastructure/Exception/ApiExceptionListener.php
@@ -17,7 +17,6 @@ final readonly class ApiExceptionListener
         }
 
         $exception = $event->getThrowable();
-        var_dump($exception->getMessage());
 
         if ($exception instanceof HttpExceptionInterface) {
             if ($exception->getPrevious() instanceof ValidationFailedException) {
