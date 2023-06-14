de8ecc7a852819b1c1a6b5fc2f322b398851ae41
Luis Cavero <luiscaverodeveloper@gmail.com>
2023-06-14 13:37:20 +0200

    Nelmio added

--- src/Checking/User/Infrastructure/EntryPoint/Api/CreateUser/CreateUserApiAction.php
+++ src/Checking/User/Infrastructure/EntryPoint/Api/CreateUser/CreateUserApiAction.php
@@ -7,8 +7,11 @@ use App\Shared\Domain\Bus\Command\CommandBus;
 use App\Shared\Infrastructure\EntryPoint\Api\ApiController;
 use App\Shared\Infrastructure\EntryPoint\Api\ApiControllerTrait;
 use App\Shared\Infrastructure\EntryPoint\Api\Response\ApiCreatedResponse;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use OpenApi\Attributes as OA;
+
 
 final readonly class CreateUserApiAction implements ApiController
 {
@@ -18,6 +21,7 @@ final readonly class CreateUserApiAction implements ApiController
     {
     }
 
+    #[OA\Post(requestBody: new OA\RequestBody(content: new OA\JsonContent(ref: new Model(type: CreateUserApiRequest::class))))]
     public function __invoke(#[MapRequestPayload] CreateUserApiRequest $request): JsonResponse
     {
         $command = CreateUserCommand::create(
